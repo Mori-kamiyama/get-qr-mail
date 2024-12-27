4cac118537e1ca79775e6caa450db746871cd7ad
mori-kamiyama <morikawa.yuta7@gmail.com>
2024-12-28 00:31:48 +0900

    取得mail数を50件に増加

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ async function getLatestEmails(auth, userName) {
         const listResp = await gmail.users.messages.list({
             userId: 'me',
             q: '-label:spam -label:trash -label:promotions -label:social subject:(入退館 OR 入館 OR 来社)',
-            maxResults: 10,
+            maxResults: 50,
         });
 
         const messages = listResp.data.messages;
